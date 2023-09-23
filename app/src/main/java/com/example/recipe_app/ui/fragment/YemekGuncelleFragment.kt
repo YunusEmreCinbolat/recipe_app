f1b4e1f76b67511abee2a7e28551b693bb649b2d
YunusEmreCinbolat@gmail.com <yunus.emre.cinbolat@gmail.com>
2023-09-24 00:43:49 +0300

    edit FragmentGuncelle.kt

--- app/src/main/java/com/example/recipe_app/ui/fragment/YemekGuncelleFragment.kt
+++ app/src/main/java/com/example/recipe_app/ui/fragment/YemekGuncelleFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.navArgs
 import com.example.recipe_app.R
 import com.example.recipe_app.databinding.FragmentYemekGuncelleBinding
 
@@ -19,11 +20,19 @@ class YemekGuncelleFragment : Fragment() {
     ): View? {
         binding= FragmentYemekGuncelleBinding.inflate(inflater,container,false)
         binding.toolbarKayit.title="Yemek Kayıt Ekranı"
+        val bundle:YemekDetayFragmentArgs by navArgs()
+        val gelenYemek=bundle.yemek
+        binding.editTextyemekAdi.setText(gelenYemek.name)
+        binding.editTextTextTarif.setText(gelenYemek.description)
         binding.buttonYemekGuncelle.setOnClickListener {
             val yemekAd=binding.editTextyemekAdi.text.trim().toString()
             val yemekTarif=binding.editTextTextTarif.text.trim().toString()
+            guncelle(gelenYemek.id,yemekAd,yemekTarif)
         }
         return binding.root
+    }
+    fun guncelle(yemekId:Int, yemekAd:String,yemekTanim:String){
+
     }
 
 }
