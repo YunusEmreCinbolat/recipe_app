5d4c1ca699a47790b87bec010ef613c5edf3395d
YunusEmreCinbolat@gmail.com <yunus.emre.cinbolat@gmail.com>
2023-09-24 01:51:50 +0300

    edit and add YemekAdapter.kt

---
+++ app/src/main/java/com/example/recipe_app/ui/adapter/YemeklerAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.recipe_app.ui.adapter
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recipe_app.data.entity.Yemek
+import com.example.recipe_app.databinding.CardTasarimBinding
+
+class YemeklerAdapter(var mContext: Context,val yemekListesi: List<Yemek>):RecyclerView.Adapter<YemeklerAdapter.CardTasarimTutucu>() {
+    inner class CardTasarimTutucu(binding: CardTasarimBinding):RecyclerView.ViewHolder(binding.root){
+        var binding:CardTasarimBinding
+        init {
+            this.binding=binding
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardTasarimTutucu {
+        val binding=CardTasarimBinding.inflate(LayoutInflater.from(mContext),parent,false)
+        return CardTasarimTutucu(binding)
+    }
+
+    override fun getItemCount(): Int {
+       return yemekListesi.size
+    }
+
+    override fun onBindViewHolder(holder: CardTasarimTutucu, position: Int) {
+        val yemek=yemekListesi.get(position)
+        holder.binding.textViewListyemekAd.text=yemek.name
+        holder.binding.cv.setOnClickListener {
+
+        }
+        holder.binding.imageViewMore
+
+    }
+}
