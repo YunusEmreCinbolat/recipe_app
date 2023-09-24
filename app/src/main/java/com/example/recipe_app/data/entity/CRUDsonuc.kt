ef6cf6e51541844fc4870c59460f8322b4851b92
YunusEmreCinbolat@gmail.com <yunus.emre.cinbolat@gmail.com>
2023-09-24 16:14:01 +0300

    create crudsonuc.kt

---
+++ app/src/main/java/com/example/recipe_app/data/entity/CRUDsonuc.kt
@@ -0,0 +1,5 @@
+package com.example.recipe_app.data.entity
+
+import com.google.gson.annotations.SerializedName
+
+data class CRUDsonuc(@SerializedName("success") var success:Int,@SerializedName("message") var message:String)
