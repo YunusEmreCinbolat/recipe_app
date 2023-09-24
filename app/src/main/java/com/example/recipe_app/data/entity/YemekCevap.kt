582bab7ca4fdccbfd0f15a85655e034d23b8d0c6
YunusEmreCinbolat@gmail.com <yunus.emre.cinbolat@gmail.com>
2023-09-24 16:11:17 +0300

    create yemekcevap.kt

---
+++ app/src/main/java/com/example/recipe_app/data/entity/YemekCevap.kt
@@ -0,0 +1,5 @@
+package com.example.recipe_app.data.entity
+
+import com.google.gson.annotations.SerializedName
+
+data class YemekCevap(@SerializedName("yemekler") var yemekler:List<Yemek>,@SerializedName("success") var success:Int)
